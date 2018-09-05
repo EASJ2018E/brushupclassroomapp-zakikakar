f64e3490cffb389065e4f613c8b80f6cd732913c
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-09-05 16:50:27 +0200

    Årstider tilføjet

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -18,16 +18,59 @@ namespace ClassRoom
             klasseRum.SemesterStart = new DateTime(2018, 4, 24);
             klasseRum.Klasseliste.Add(new Studerende("Arlid", 4, 24));
             klasseRum.Klasseliste.Add(new Studerende("Benja", 6, 18));
-            
-            Console.WriteLine(klasseRum.KlasseNavn);
-            Console.WriteLine(klasseRum.SemesterStart);
-
+            klasseRum.Klasseliste.Add(new Studerende("Joey", 12, 6));
 
             foreach (Studerende elev in klasseRum.Klasseliste)
             {
                 Console.WriteLine(elev.Navn + elev.Fødselsdag + elev.Fødselsmåned);
+
+                switch (elev.Fødselsmåned)
+                {
+                    case 1:
+                        Console.WriteLine("Vinter");
+                        break;
+                    case 2:
+                        Console.WriteLine("Vinter");
+                        break;
+                    case 12:
+                        Console.WriteLine("Vinter");
+                        break;
+                    case 3:
+                        Console.WriteLine("Forår");
+                        break;
+                    case 4:
+                        Console.WriteLine("Forår");
+                        break;
+                    case 5:
+                        Console.WriteLine("Forår");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sommer");
+                        break;
+                    case 7:
+                        Console.WriteLine("Sommer");
+                        break;
+                    case 8:
+                        Console.WriteLine("Sommer");
+                        break;
+                    case 9:
+                        Console.WriteLine("Efterår");
+                        break;
+                    case 10:
+                        Console.WriteLine("Efterår");
+                        break;
+                    case 11:
+                        Console.WriteLine("Efterår");
+                        break;
+
+                }
+
+                Console.WriteLine(klasseRum.KlasseNavn);
+                Console.WriteLine(klasseRum.SemesterStart);
+
             }
             Console.ReadKey();
         }
     }
 }
+
