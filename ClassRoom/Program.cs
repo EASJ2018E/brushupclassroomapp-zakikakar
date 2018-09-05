784d188202d4e6f1853fd1296eba495a606809fd
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-09-05 16:58:43 +0200

    årstider tiløfjet

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -26,43 +26,21 @@ namespace ClassRoom
 
                 switch (elev.Fødselsmåned)
                 {
-                    case 1:
+                    case 1: case 2: case 12:
                         Console.WriteLine("Vinter");
                         break;
-                    case 2:
-                        Console.WriteLine("Vinter");
-                        break;
-                    case 12:
-                        Console.WriteLine("Vinter");
-                        break;
-                    case 3:
+                    case 3: case 4: case 5:
                         Console.WriteLine("Forår");
                         break;
-                    case 4:
-                        Console.WriteLine("Forår");
-                        break;
-                    case 5:
-                        Console.WriteLine("Forår");
-                        break;
-                    case 6:
+                    case 6: case 7: case 8:
                         Console.WriteLine("Sommer");
                         break;
-                    case 7:
-                        Console.WriteLine("Sommer");
-                        break;
-                    case 8:
-                        Console.WriteLine("Sommer");
-                        break;
-                    case 9:
+                    case 9: case 10: case 11:
                         Console.WriteLine("Efterår");
                         break;
-                    case 10:
-                        Console.WriteLine("Efterår");
+                    default:
+                        Console.WriteLine("Ukendt fødselsmåned");
                         break;
-                    case 11:
-                        Console.WriteLine("Efterår");
-                        break;
-
                 }
 
                 Console.WriteLine(klasseRum.KlasseNavn);
