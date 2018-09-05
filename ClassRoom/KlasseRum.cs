f8a7efdfffaed6f42f9d14a115f85593c2aae7c9
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-09-05 14:28:53 +0200

    klasserum tilføjet

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+        public List<Studerende> Klasseliste { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+        }
+    }
+}
